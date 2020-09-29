b9f2e0ef9e67f86f7d725ef380087b4c6551f1ff
jeodell <jeodell@ncsu.edu>
2020-09-29 12:24:31 -0400

    Updates

---
+++ precache-manifest.85faab617f58f344aa98ba1001291f12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f77569a5995e3912b3ad6bdb03845b3",
+    "url": "/SpotipyNewReleases/index.html"
+  },
+  {
+    "revision": "e307fcca7bde16fa3dba",
+    "url": "/SpotipyNewReleases/static/css/main.87ab6d64.chunk.css"
+  },
+  {
+    "revision": "84a2c6e3b257334b3831",
+    "url": "/SpotipyNewReleases/static/js/2.7144b697.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/SpotipyNewReleases/static/js/2.7144b697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e307fcca7bde16fa3dba",
+    "url": "/SpotipyNewReleases/static/js/main.76e49fa5.chunk.js"
+  },
+  {
+    "revision": "cd0d1142637497f784db",
+    "url": "/SpotipyNewReleases/static/js/runtime-main.14a57aa4.js"
+  }
+]);
