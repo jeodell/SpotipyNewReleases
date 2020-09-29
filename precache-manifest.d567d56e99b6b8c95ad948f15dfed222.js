136daafe84c61415e0ce591f17e952f8fa9ba9a9
jeodell <jeodell@ncsu.edu>
2020-09-29 14:31:45 -0400

    Updates

--- precache-manifest.d567d56e99b6b8c95ad948f15dfed222.js
+++ precache-manifest.d567d56e99b6b8c95ad948f15dfed222.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "328ab764f224fcefcbb7ec0ec6e72cc6",
+    "revision": "fd4eed53dbf3627b640679a992784ded",
     "url": "/SpotipyNewReleases/index.html"
   },
   {
-    "revision": "5a6fc326d92b293e5393",
+    "revision": "d14bf175b519030def32",
     "url": "/SpotipyNewReleases/static/css/main.87ab6d64.chunk.css"
   },
   {
-    "revision": "84a2c6e3b257334b3831",
-    "url": "/SpotipyNewReleases/static/js/2.7144b697.chunk.js"
+    "revision": "8c512ab96f77a4961083",
+    "url": "/SpotipyNewReleases/static/js/2.314ad267.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/SpotipyNewReleases/static/js/2.7144b697.chunk.js.LICENSE.txt"
+    "url": "/SpotipyNewReleases/static/js/2.314ad267.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5a6fc326d92b293e5393",
-    "url": "/SpotipyNewReleases/static/js/main.fd140e5c.chunk.js"
+    "revision": "d14bf175b519030def32",
+    "url": "/SpotipyNewReleases/static/js/main.a5947f10.chunk.js"
   },
   {
     "revision": "cd0d1142637497f784db",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SpotipyNewReleases/precache-manifest.54979fa751a9c9cb4a3b227ecd1755a1.js"
+  "/SpotipyNewReleases/precache-manifest.d567d56e99b6b8c95ad948f15dfed222.js"
 );
 
 self.addEventListener('message', (event) => {
